207724523b8326220dcc48f419196ecf3365aa78
DanielHealy <danielhealyhosting3@gmail.com>
2022-08-20 13:52:37 +0100

    Register and Signup forms now discrete with unique handlers

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ Modal.setAppElement("#modal");
 
 function Navbar() {
 
-    const [modalType, setModalType] = useState(null)
+    const [modalType, setModalType] = useState("login")
 
     const [modalIsOpen, setIsOpen] = useState(false);
 
@@ -25,11 +25,17 @@ function Navbar() {
         },
     };
 
-    function openModal() {
+    function openRegisterModal() {
+      setModalType("register")  
       setIsOpen(true);
     }
 
-    function closeModal() {
+    function openLoginModal() {
+      setModalType("login");
+      setIsOpen(true);
+    }
+
+    function closeModal() {  
       setIsOpen(false);
     }
    
@@ -51,7 +57,7 @@ function Navbar() {
 
     console.log(formData)
 
-    const onSubmit = (e) => {
+    const onSubmitRegister = (e) => {
         e.preventDefault();
 
         if (password !== password2) {
@@ -65,84 +71,160 @@ function Navbar() {
         }
     }
 
+    const onSubmitLogin = (e) => {
+        e.preventDefault();
 
-  return (
-    <div className={styles.navContainer}>
-      <div className="logoContainer">
-        <Image src="/mockLogo.svg" width={140} height={140} alt="mockLogo" />
-      </div>
-      <div className={styles.buttonWrapper}>
-        <p className={styles.navButton} onClick={openModal}>
-          Sign-up
-        </p>
-        <p className={styles.navButton} onClick={openModal}>
-          Login
-        </p>
-        <div id="modal">
-          <div>
-            <Modal
-              isOpen={modalIsOpen}
-              onRequestClose={closeModal}
-              style={customStyles}
-              contentLabel="Example Modal"
-            >
-              <section className={styles.section}>
-                <form onSubmit={onSubmit} className={styles.form}>
-                  <div className={styles.formGroup}>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      value={name}
-                      onChange={onChange}
-                      placeholder="Enter your name"
-                      required
-                    />
-                  </div>
-                  <div className={styles.formGroup}>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      value={email}
-                      onChange={onChange}
-                      placeholder="Enter your email"
-                      required
-                    />
-                  </div>
-                  <div className={styles.formGroup}>
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      value={password}
-                      onChange={onChange}
-                      placeholder="Enter password"
-                      required
-                    />
-                  </div>
-                  <div className={styles.formGroup}>
-                    <input
-                      type="password"
-                      id="password2"
-                      name="password2"
-                      value={password2}
-                      onChange={onChange}
-                      placeholder="Confirm password"
-                      required
-                    />
-                  </div>
-                  <div className={styles.formGroup}>
-                    <button className={styles.btn}>Submit</button>
-                  </div>
-                </form>
-              </section>
-            </Modal>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+        if (password !== password2) {
+            console.log("Passwords do not match");
+        } else {
+            const userData = {
+            name,
+            email,
+            password,
+            };
+        }
+    };
+
+    if (modalType === "register") {
+        return (
+            <div className={styles.navContainer}>
+                <div className="logoContainer">
+                    <Image src="/mockLogo.svg" width={140} height={140} alt="mockLogo" />
+                </div>
+                <div className={styles.buttonWrapper}>
+                    <p className={styles.navButton} onClick={openRegisterModal}>
+                        Sign-up
+                    </p>
+                    <p className={styles.navButton} onClick={openLoginModal}>
+                        Login
+                    </p>
+                    <div id="modal">
+                        <div>
+                            <Modal
+                                isOpen={modalIsOpen}
+                                onRequestClose={closeModal}
+                                style={customStyles}
+                                contentLabel="Example Modal"
+                            >
+                                <section className={styles.section}>
+                                    <form onSubmit={onSubmitRegister} className={styles.form}>
+                                        <div className={styles.formGroup}>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                name="name"
+                                                value={name}
+                                                onChange={onChange}
+                                                placeholder="Enter your name"
+                                                required
+                                            />
+                                        </div>
+                                        <div className={styles.formGroup}>
+                                            <input
+                                                type="email"
+                                                id="email"
+                                                name="email"
+                                                value={email}
+                                                onChange={onChange}
+                                                placeholder="Enter your email"
+                                                required
+                                            />
+                                        </div>
+                                        <div className={styles.formGroup}>
+                                            <input
+                                                type="password"
+                                                id="password"
+                                                name="password"
+                                                value={password}
+                                                onChange={onChange}
+                                                placeholder="Enter password"
+                                                required
+                                            />
+                                        </div>
+                                        <div className={styles.formGroup}>
+                                            <input
+                                                type="password"
+                                                id="password2"
+                                                name="password2"
+                                                value={password2}
+                                                onChange={onChange}
+                                                placeholder="Confirm password"
+                                                required
+                                            />
+                                        </div>
+                                        <div className={styles.formGroup}>
+                                            <button className={styles.btn}>Submit</button>
+                                        </div>
+                                    </form>
+                                </section>
+                            </Modal>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    } else if (modalType === "login") {
+            return (
+            <div className={styles.navContainer}>
+                <div className="logoContainer">
+                <Image
+                    src="/mockLogo.svg"
+                    width={140}
+                    height={140}
+                    alt="mockLogo"
+                />
+                </div>
+                <div className={styles.buttonWrapper}>
+                <p className={styles.navButton} onClick={openRegisterModal}>
+                    Sign-up
+                </p>
+                <p className={styles.navButton} onClick={openRegisterModal}>
+                    Login
+                </p>
+                <div id="modal">
+                    <div>
+                    <Modal
+                        isOpen={modalIsOpen}
+                        onRequestClose={closeModal}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                    >
+                        <section className={styles.section}>
+                        <form onSubmit={onSubmitLogin} className={styles.form}>
+                            <div className={styles.formGroup}>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={email}
+                                onChange={onChange}
+                                placeholder="Enter your email"
+                                required
+                            />
+                            </div>
+                            <div className={styles.formGroup}>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                value={password}
+                                onChange={onChange}
+                                placeholder="Enter password"
+                                required
+                            />
+                            </div>
+                            <div className={styles.formGroup}>
+                            <button className={styles.btn}>Submit</button>
+                            </div>
+                        </form>
+                        </section>
+                    </Modal>
+                    </div>
+                </div>
+                </div>
+            </div>
+        );   
+    }
 }
 
 export default Navbar
